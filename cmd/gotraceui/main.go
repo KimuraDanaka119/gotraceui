KimuraDanaka119/gotraceui

commit ec795e2112b719391f7880de4104fd276e0a798a

Add hotkey for opening heatmap

cmd/gotraceui/main.go
@@ -2471,10 +2471,15 @@ func (w *MainWindow) Run(win *app.Window) error {
 					for _, ev := range gtx.Events(&shortcuts) {
 						switch ev := ev.(type) {
 						case key.Event:
-							if ev.State == key.Press && ev.Name == "G" && ww == nil {
-								ww = theme.NewListWindow[*Goroutine](w.theme)
-								ww.SetItems(w.trace.gs)
-								ww.BuildFilter = newGoroutineFilter
+							if ev.State == key.Press {
+								switch ev.Name {
+								case "G":
+									ww = theme.NewListWindow[*Goroutine](w.theme)
+									ww.SetItems(w.trace.gs)
+									ww.BuildFilter = newGoroutineFilter
+								case "H":
+									w.openHeatmap()
+								}
 							}
 						}
 					}
@@ -2483,7 +2488,7 @@ func (w *MainWindow) Run(win *app.Window) error {
 						w.openGoroutineWindow(g, w.trace)
 					}
 
-					key.InputOp{Tag: &shortcuts, Keys: "G"}.Add(gtx.Ops)
+					key.InputOp{Tag: &shortcuts, Keys: "G|H"}.Add(gtx.Ops)
 
 					if ww != nil {
 						if item, ok := ww.Confirmed(); ok {

commit 0f681cc09e93dd9dc6ab6b1ba47d5d5e0ba39bc5

Don't respond to shortcuts when list window is open
This fixes a regression introduced in
ec795e2112b719391f7880de4104fd276e0a798a.

Closes: gh-33

cmd/gotraceui/main.go
@@ -480,7 +480,7 @@ func (w *MainWindow) Run(win *app.Window) error {
 					for _, ev := range gtx.Events(&shortcuts) {
 						switch ev := ev.(type) {
 						case key.Event:
-							if ev.State == key.Press {
+							if ev.State == key.Press && w.ww == nil {
 								switch ev.Name {
 								case "G":
 									w.ww = theme.NewListWindow[*Goroutine](w.theme)